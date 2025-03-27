1e8a0566403721e9974438e8eadf9782f71f50d6
mamingze <1132990723@qq.com>
2025-03-27 11:23:06 +0800

    增加打印

--- src/main/java/com/chenxu/physical/theatre/bussiness/controller/CourseController.java
+++ src/main/java/com/chenxu/physical/theatre/bussiness/controller/CourseController.java
@@ -146,6 +146,7 @@ public class CourseController {
             apiWeekCourseModel.setList(tempList);
             apiWeekCourseModels.add(apiWeekCourseModel);
         }
+        logger.info("checkTwoWeekCourses::apiWeekCourseModels = [{}]", apiWeekCourseModels);
         return apiWeekCourseModels;
 
     }
