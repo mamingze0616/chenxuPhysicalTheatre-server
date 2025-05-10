8ad760811b1999c4daad97724e0e21145736770d
mamingze <1132990723@qq.com>
2025-05-11 04:29:39 +0800

    unifiedOrder 支付模块写统一下单木块

---
+++ src/main/java/com/chenxu/physical/theatre/bussiness/controller/PayController.java
@@ -0,0 +1,64 @@
+package com.chenxu.physical.theatre.bussiness.controller;
+
+import com.chenxu.physical.theatre.bussiness.constant.Constant;
+import com.chenxu.physical.theatre.bussiness.dto.ApiResponse;
+import com.chenxu.physical.theatre.bussiness.service.PayService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mamingze
+ * @version 1.0
+ * @title PayController
+ * @description
+ * @create 2025/5/11 03:59
+ */
+@RestController
+@RequestMapping("/pay")
+public class PayController {
+    private static final Logger logger = LoggerFactory.getLogger(PayController.class);
+    @Autowired
+    private PayService payService;
+
+    @PostMapping("/unifiedOrder")
+    public ApiResponse unifiedOrder(@RequestHeader(value = "X-WX-OPENID", required = false, defaultValue = "none")
+                                    String openid, HttpServletRequest request) {
+
+        String ipAddress = request.getHeader("X-Forwarded-For");
+
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP
+        if (ipAddress != null && ipAddress.contains(",")) {
+            ipAddress = ipAddress.split(",")[0];
+        }
+        logger.info("openid:{},ipAddress:{}", openid, ipAddress);
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(Constant.APIRESPONSE_FAIL);
+        try {
+            apiResponse.setCode(Constant.APIRESPONSE_SUCCESS);
+            apiResponse.setData(payService.unifiedOrder(openid, "测试商品", "123456", 100, ipAddress));
+        } catch (Exception e) {
+            e.printStackTrace();
+            apiResponse.setCode(Constant.APIRESPONSE_FAIL);
+            apiResponse.setErrorMsg("下单失败");
+
+        }
+        return apiResponse;
+    }
+
+}
---
+++ src/main/java/com/chenxu/physical/theatre/bussiness/service/PayService.java
@@ -0,0 +1,72 @@
+package com.chenxu.physical.theatre.bussiness.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author mamingze
+ * @version 1.0
+ * @title PayService
+ * @description
+ * @create 2025/5/11 02:33
+ */
+@Service
+public class PayService {
+    private static final Logger logger = LoggerFactory.getLogger(PayService.class);
+    @Value("${pay.unifiedOrder.url}")
+    private String unifiedOrderUrl;
+    @Value("${wx.env}")
+    private String env;
+    @Value("${wx.mch}")
+    private String mch;
+    @Value("${pay.unifiedOrder.callback.service}")
+    private String service;
+    @Value("${pay.unifiedOrder.callback.path}")
+    private String path;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    /**
+     * @param openid
+     * @param body
+     * @param outTradeNo 内部订单号
+     * @param totalFee
+     * @return
+     */
+    public String unifiedOrder(String openid, String body, String outTradeNo, int totalFee, String spbillCreateIp) {
+
+        try {
+            Map<String, Object> requestBody = new HashMap<>();
+            requestBody.put("body", body);
+            requestBody.put("openid", openid);
+            requestBody.put("out_trade_no", outTradeNo);
+            requestBody.put("spbill_create_ip", spbillCreateIp);
+            requestBody.put("env_id", env);
+            requestBody.put("sub_mch_id", mch);
+            requestBody.put("total_fee", totalFee);
+            requestBody.put("callback_type", 2);
+            Map<String, String> container = new HashMap<>();
+            container.put("service", service);
+            container.put("path", path);
+            requestBody.put("container", container);
+
+            String responseText = restTemplate.postForObject(unifiedOrderUrl, requestBody, String.class);
+            logger.info("接口返回:[{}]", responseText);
+            return responseText;
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("统一下单失败:" + e.getMessage());
+            throw new RuntimeException("统一下单失败");
+        }
+
+    }
+
+}
