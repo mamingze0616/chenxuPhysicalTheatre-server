420d2508d8476df70db643cd1f37e5fd4fc2a444
mamingze <1132990723@qq.com>
2025-05-16 04:07:42 +0800

    checkUserIsMemberAnd空优惠券判断

--- src/main/java/com/chenxu/physical/theatre/bussiness/service/MemberShipService.java
+++ src/main/java/com/chenxu/physical/theatre/bussiness/service/MemberShipService.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * @author mamingze
@@ -48,14 +49,17 @@ public class MemberShipService {
                 throw new RuntimeException("已经是会员");
             }
             //查询用户优惠券,暂时只能使用一个
-            TUserCoupons userCoupons = tUserCouponsService.getById(tUserOrder.getCouponIds());
-            if (userCoupons == null) {
-                throw new RuntimeException("优惠券不存在");
+            if (StringUtils.hasText(tUserOrder.getCouponIds())) {
+                TUserCoupons userCoupons = tUserCouponsService.getById(tUserOrder.getCouponIds());
+                if (userCoupons == null) {
+                    throw new RuntimeException("优惠券不存在");
+                }
+                //判断是否已经使用
+                if (userCoupons.getStatus() == TUserCouponsStatus.FINISHED) {
+                    throw new RuntimeException("优惠券已经使用");
+                }
             }
-            //判断是否已经使用
-            if (userCoupons.getStatus() == TUserCouponsStatus.FINISHED) {
-                throw new RuntimeException("优惠券已经使用");
-            }
+
             return true;
         } catch (Exception e) {
             logger.error(e.getMessage());
@@ -69,19 +73,16 @@ public class MemberShipService {
         TPayOrder payOrder = new TPayOrder();
         try {
             //第一步:预创建一个空白支付订单
-            TPayOrder prePayOrder = payService.preCreateMembershipPayOrder(tUserOrder,
-                    "升级会员订单");
+            TPayOrder prePayOrder = payService.preCreateMembershipPayOrder(tUserOrder, "升级会员订单");
             //创建一个用户升级订单,绑定预创建的支付订单
             tUserOrder.setPayOrderId(prePayOrder.getId());
             tUserOrder.setStatus(TUserOrderStatus.UNPAID);
             //第二步:保存用户升级订单
             tUserOrderService.save(tUserOrder);
             //第三步:给微信官网发送请求，获取预支付订单
-            payOrder = payService.unifiedOrder(prePayOrder,
-                    tUserOrder.getId(), TPayOrderType.MEMBERSHIP, spbillCreateIp);
+            payOrder = payService.unifiedOrder(prePayOrder, tUserOrder.getId(), TPayOrderType.MEMBERSHIP, spbillCreateIp);
             payOrder.setUserOrder(tUserOrder);
-            tUserCouponsService.lambdaUpdate().eq(TUserCoupons::getId, tUserOrder.getCouponIds())
-                    .set(TUserCoupons::getStatus, TUserCouponsStatus.FINISHED.getCode()).update();
+            tUserCouponsService.lambdaUpdate().eq(TUserCoupons::getId, tUserOrder.getCouponIds()).set(TUserCoupons::getStatus, TUserCouponsStatus.FINISHED.getCode()).update();
         } catch (Exception e) {
             logger.error(e.getMessage());
         } finally {
@@ -99,11 +100,9 @@ public class MemberShipService {
                 throw new RuntimeException("用户升级订单不存在");
             }
             //更新订单状态为已支付
-            tUserOrderService.lambdaUpdate().eq(TUserOrder::getId, tUserOrder.getId())
-                    .set(TUserOrder::getStatus, TUserOrderStatus.PAID.getCode()).update();
+            tUserOrderService.lambdaUpdate().eq(TUserOrder::getId, tUserOrder.getId()).set(TUserOrder::getStatus, TUserOrderStatus.PAID.getCode()).update();
             //修改用户状态为会员用户
-            tUserService.lambdaUpdate().eq(TUser::getId, userOrder.getUserId())
-                    .set(TUser::getType, TUserType.MEMBER.getCode()).update();
+            tUserService.lambdaUpdate().eq(TUser::getId, userOrder.getUserId()).set(TUser::getType, TUserType.MEMBER.getCode()).update();
             return userOrder;
             //查询用户升级订单
         } catch (Exception e) {
