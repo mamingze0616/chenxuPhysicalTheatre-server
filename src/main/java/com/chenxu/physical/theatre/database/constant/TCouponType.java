ae5403423157b8da49c8acd2a9070673fe1dc055
mamingze <1132990723@qq.com>
2025-06-08 15:38:59 +0800

    TCouponType增加优惠券类型

--- src/main/java/com/chenxu/physical/theatre/database/constant/TCouponType.java
+++ src/main/java/com/chenxu/physical/theatre/database/constant/TCouponType.java
@@ -11,9 +11,11 @@ import com.fasterxml.jackson.annotation.JsonValue;
  * @create 2025/5/7 00:14
  */
 public enum TCouponType {
-    //1:课程;2;折扣
+    //1:课程;2;折扣;3:委培制定优惠券;4:购买课程指定优惠券
     COUPON_TYPE_COURSE(1, "课程"),
-    COUPON_TYPE_DISCOUNT(2, "折扣");
+    COUPON_TYPE_DISCOUNT(2, "折扣"),
+    COUPON_TYPE_WORK_EMPLOYEE(3, "委培指定优惠券"),
+    COUPON_TYPE_BUY_COURSE(4, "购买课程指定优惠券");
 
     @EnumValue
     @JsonValue
